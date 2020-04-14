b33ddadf450f1232c6c657ae66fa7c375a66915d
Ho Jin <ycool37@naver.com>
2020-04-14 17:52:50 +0900

    액션태그활용2

--- src/sec01/ex02/MemberBean.java
+++ src/sec01/ex02/MemberBean.java
@@ -5,12 +5,27 @@ import java.sql.Date;
 public class MemberBean {
 	private int number;//기본숫자1을 저장 
 	
-	private Integer num; <-- new Integer("1");
-	
+	private Integer num; //<-- 1이 오토박싱되어 new Integer("1");
+//										//박싱
 	private String id, pwd, name, email;
 	private Date joinDate;
 	
 	//회원의 주소를 저장하는 Address클래스 타입의 변수 선언 
 	private Address addr;
+	
+	public MemberBean() {}
+
+	public MemberBean(int number, Integer num, String id, String pwd, String name, String email, Date joinDate,
+			Address addr) {
+		super();
+		this.number = number;
+		this.num = num;
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.email = email;
+		this.joinDate = joinDate;
+		this.addr = addr;
+	}
 	
 }
